70f56841e60fe455694178606db98a78445d270b
kleinric <kleinric@gmail.com>
2015-07-15 15:27:55 +0200

    Added Handout 3 Files

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include "window.h"
+#include "texture.h"
+#include "tile.h"
+
+using namespace std;
+
+int main()
+{
+    // SpriteSheet Filename
+    string spriteFilename = SPRITEFILENAME; // Leave this line
+
+    // Setup and Load Texture object here
+    bool quit = false;
+
+    while(!quit){
+        // Handle any SDL Events
+        // Such as resize, clicking the close button,
+        //  and process and key press events.
+
+        // Update the Game State Information
+
+        // Draw the current state to the screen.
+    }
+
+    return 0;
+}
