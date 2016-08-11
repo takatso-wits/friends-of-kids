26d34f4d0d9962c6e3578583e1f116adaf9df08c
Takatso <569869@students.wits.ac.za>
2016-08-11 04:58:04 +0200

    Animate Sprite in Main Loop.

--- main.cpp
+++ main.cpp
@@ -8,23 +8,56 @@
 
 using namespace std;
 
+ Tile pm(0,0,{ {1,1},{1,2}, {1,1}, {1,3} }, Pacman, 1,1);
+ int frame = 0;
+ 
+ //vector<Tile> testTiles = getTestTiles();
+ 
 int main()
 {
+    Texture myTexture;
+  
     // SpriteSheet Filename
     string spriteFilename = SPRITEFILENAME; // Leave this line
 
+    myTexture.loadFile(spriteFilename, 20, 20); 
+    
     // Setup and Load Texture object here
     bool quit = false;
 
     while(!quit){
         // Handle any SDL Events
-        // Such as resize, clicking the close button,
-        //  and process and key press events.
-
-        // Update the Game State Information
-
-        // Draw the current state to the screen.
+        SDL_Event e;
+        while(SDL_PollEvent(&e)){
+          if(e.type == SDL_QUIT){
+          quit = true;
+        }
+      }
+        // Set background to black
+        SDL_SetRenderDrawColor(myTexture.myWin.sdlRenderer, 0, 0, 0, 0xFF);
+        // Clear the renderer
+        SDL_RenderClear(myTexture.myWin.sdlRenderer);
+        // Render the tile
+        
+       pm.render(&myTexture, frame);
+       
+       //loop to iterate through testTiles
+       
+      /*for(int i = 0; i < testTiles.size(); i++)
+      {
+        
+        testTiles[i].render(&myTexture, i);
+         
+      }
+        */
+       
+        
+        SDL_RenderPresent(myTexture.myWin.sdlRenderer);
+        frame++;
+        this_thread::sleep_for(chrono::milliseconds(75));
     }
+    
+  
 
     return 0;
 }
